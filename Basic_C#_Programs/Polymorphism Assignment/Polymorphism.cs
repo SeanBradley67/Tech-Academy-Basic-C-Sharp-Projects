088c071b472cdfbd841955c5b75c2dadb4e6aee5
Sean Bradley <s_bradley67@hotmail.com>
2025-02-12 15:25:06 -0800

    Polymorphism Assignment

---
+++ Basic_C#_Programs/Polymorphism Assignment/Polymorphism.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Polymorphism_Assignment
+{
+    public interface IQuittable
+    {
+        void Quit();
+    }
+
+    // Abstract class Person
+    public abstract class Person
+    {
+        // Properties for first and last name
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Abstract method SayName, to be implemented in derived classes
+        public abstract void SayName();
+    }
+
+    // Employee class inherits from Person and implements IQuittable
+    public class Employee : Person, IQuittable
+    {
+        // Implement SayName method from Person
+        public override void SayName()
+        {
+            Console.WriteLine($"Full Name: {FirstName} {LastName}");
+        }
+
+        // Implement Quit method from IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+        }
+    }
+}
---
+++ Basic_C#_Programs/Polymorphism Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Polymorphism_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an Employee object and set first and last name
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call SayName method
+            employee.SayName();
+
+            // Use polymorphism: Create an IQuittable reference and assign Employee object
+            IQuittable quittableEmployee = employee;
+
+            // Call Quit method via the IQuittable interface
+            quittableEmployee.Quit();
+
+            // Prevent console from closing immediately
+            Console.ReadLine();
+        }
+    }
+}
